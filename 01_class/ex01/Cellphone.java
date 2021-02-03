d1e2aa4b601c3adb11c8f6bc9ccad4e771f2ea35
marobiana <69413030+marobiana@users.noreply.github.com>
2021-02-03 17:04:44 +0900

    Update Cellphone.java

---
+++ 01_class/ex01/Cellphone.java
@@ -0,0 +1,19 @@
+package class_ex01;
+
+// 설계도
+public class Cellphone {
+	// 필드
+	String model;
+	int price;
+	String color;
+	String maker;
+	
+	// 메소드
+	public void call() {
+		System.out.println("전화하기");
+	}
+	
+	public void camera() {
+		System.out.println("카메라 찍기");
+	}
+}
